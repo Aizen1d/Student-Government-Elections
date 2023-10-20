7b68ae77c285c0a3febe710de785bff15ca04bb2
Aizen1d <ddreataza@gmail.com>
2023-10-20 23:17:11 +0800

    Tanstack query use for fetching rule/guideline data

--- resources/js/Pages/RulesAndGuidelines.vue
+++ resources/js/Pages/RulesAndGuidelines.vue
@@ -4,118 +4,114 @@
     <div class="container-fluid main">
         <div class="sector mx-5">
             <h1 class="my-4 rules-label">RULES</h1>
-
-            <div class="details" v-for="(rule, index) in rules" :key="index">
-                <span>{{ rule.count }} {{ rule.title }}</span>
-                <p class="my-2">{{ rule.body }}</p>
+            
+            <div class="details" v-if="isRuleSucess" v-for="(rule, index) in rulesData.rules" :key="index">
+                <span>{{ rule.count }} {{ rule.RuleTitle }}</span>
+                <p class="my-2">{{ rule.RuleBody }}</p>
             </div>
         </div>
 
         <div class="sector mx-5">
             <h1 class="my-4 guidelines-label">GUIDELINES</h1>
 
-            <div class="details" v-for="(guideline, index) in guidelines" :key="index">
-                <span>{{ guideline.count }} {{ guideline.title }}</span>
-                <p class="my-2">{{ guideline.body }}</p>
+            <div class="details" v-if="isGuidelineSucess" v-for="(guideline, index) in guidelinesData.guidelines" :key="index">
+                <span>{{ guideline.count }} {{ guideline.GuidelineTitle }}</span>
+                <p class="my-2">{{ guideline.GuidelineBody }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Standards from '../Shared/Standards.vue'
-    import Navbar from '../Shared/Navbar.vue'
+    import Navbar from "../Shared/Navbar.vue";
+    import Standards from "../Shared/Standards.vue";
 
-    import axios from 'axios'
-    import { ref } from 'vue'
+    import { useQuery } from "@tanstack/vue-query";
+    import { ref } from "vue";
+    import axios from "axios";
+
+    const fetchRules = async () => {
+        const response = await axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/rule/all`);
+        return response.data;
+    };
+
+    const fetchGuidelines = async () => {
+        const response = await axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/guideline/all`);
+        return response.data;
+    };
 
     export default {
         setup() {
-            const rules = ref([]);
-            const guidelines = ref([]);
+            const page = ref(1);
+            const { data: rulesData, 
+                    isLoading: isRuleLoading, 
+                    isSuccess: isRuleSucess,
+                    isError: isRuleError, } = 
+                    useQuery({
+                        queryKey: ["rules", page],
+                        queryFn: fetchRules,
+                        staleTime: 1000 * 60 * 5, // data becomes stale after 5 minutes
+                    });
 
-            return {
-                rules,
-                guidelines
-            }
+            const { data: guidelinesData, 
+                    isLoading: isGuidelineLoading, 
+                    isSuccess: isGuidelineSucess, 
+                    isError: isGuidelineError, } = 
+                    useQuery({
+                        queryKey: ["guidelines", page],
+                        queryFn: fetchGuidelines,
+                        staleTime: 1000 * 60 * 5, // data becomes stale after 5 minutes
+                    });
+
+            return { 
+                rulesData,
+                isRuleLoading,
+                isRuleSucess,
+                isRuleError,
+                guidelinesData,
+                isGuidelineLoading,
+                isGuidelineSucess,
+                isGuidelineError,
+            };
         },
         components: {
-            Standards,
             Navbar,
+            Standards,
         },
-        created() {
-            this.fetchRules();
-            this.fetchGuidelines();
-        },
-        methods: {
-            fetchRules() {
-                axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/rule/all`)
-                    .then(response => {
-                    console.log(`Get all rules successful. Duration: ${response.duration}ms`)
-
-                    const rules = response.data.rules.map(rule => ({
-                        id: rule.RuleId,
-                        count: "Rule #" + rule.count,
-                        type: rule.type,
-                        title: rule.RuleTitle,
-                        body: rule.RuleBody
-                    }));
-
-                    this.rules = rules;
-                });
-            },
-
-            fetchGuidelines() {
-                axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/guideline/all`)
-                    .then(response => {
-                    console.log(`Get all guidelines successful. Duration: ${response.duration}ms`)
-
-                    const guidelines = response.data.guidelines.map(guideline => ({
-                            id: guideline.GuideId,
-                            count: "Guideline #" + guideline.count,
-                            type: guideline.type,
-                            title: guideline.GuidelineTitle,
-                            body: guideline.GuidelineBody
-                        }));
-
-                    this.guidelines = guidelines;
-                });
-            },
-        }
-    }
+    };
 </script>
 
 <style scoped>
-    .main{
-        font-size: 20px;
-        margin-top: 3%;
-    }
+.main {
+    font-size: 20px;
+    margin-top: 3%;
+}
 
-    .main h1{
-        font-size: 20px;
-        letter-spacing: 3px;
-        font-weight: 800;
-    }
+.main h1 {
+    font-size: 20px;
+    letter-spacing: 3px;
+    font-weight: 800;
+}
 
-    .sector{
-        margin: 3% 0 3% 0;
-    }
+.sector {
+    margin: 3% 0 3% 0;
+}
 
-    .rules-label{
-        font-size: 1.5em !important;
-        letter-spacing: 0.2em !important;
-    }
+.rules-label {
+    font-size: 1.5em !important;
+    letter-spacing: 0.2em !important;
+}
 
-    .guidelines-label{
-        font-size: 1.5em !important;
-        letter-spacing: 0.2em !important;
-    }
+.guidelines-label {
+    font-size: 1.5em !important;
+    letter-spacing: 0.2em !important;
+}
 
-    .details{
-        margin-top: 2%;
-    }
+.details {
+    margin-top: 2%;
+}
 
-    .details span{
-        font-weight: 700;
-    }
+.details span {
+    font-weight: 700;
+}
 </style>
