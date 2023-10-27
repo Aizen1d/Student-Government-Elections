4db48173030e36ba3aaabdc8c920dd9f8c595953
Aizen1d <ddreataza@gmail.com>
2023-10-28 00:32:18 +0800

    Added action button component

---
+++ resources/js/Shared/ActionButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="$style.button">
+       <slot></slot>
+    </button>
+</template>
+
+<style module>
+    .button{
+        padding: 10px 50px 10px 50px;
+        border: transparent;
+        border-radius: 10px;
+        background-color: #B90321;
+        color: white;
+        font-size: 17px;
+        font-family: 'Source Sans Pro', sans-serif !important;
+    }
+    .button:hover{
+        background-color: #a6021d;
+    }
+    .button:disabled{
+        background-color: #cccccc;
+    }
+
+</style>
