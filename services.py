c5cb7f52d900b7f3560d5b7885707f471c88794d
Aizen1d <ddreataza@gmail.com>
2023-11-17 01:22:44 +0800

    Emailing for coc/partylist status on approve/reject

--- services.py
+++ services.py
@@ -129,3 +129,100 @@ def send_pass_code_manual_email(student_number, student_email, pass_code):
 
     except Exception as e:
         print(e)
+
+def send_coc_status_email(student_number, student_email, status, role, election_name):
+    # Set up the SMTP server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
+
+    # Create the email
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL
+    msg['To'] = student_email
+
+    if status == "Approved":
+        msg['Subject'] = "Confirmation of Candidacy Approval"
+    else:
+        msg['Subject'] = "Regret on Candidacy Application"
+
+    accept = f"""
+        <html>
+        <body>
+            <p>Dear, Student {student_number}</p>
+            <p style="margin-top: 0.2em; margin-bottom: 0.2em;">Congratulations! We are pleased to inform you that your candidacy for running <strong>{role}</strong> on <strong>{election_name}</strong> has been approved.</p>
+            <p style="color: black;">Your enthusiasm and commitment are truly commendable, and we look forward to seeing the positive impact you will make in your role.</p>
+        </body>
+        </html>
+    """
+
+    reject = f"""
+        <html>
+        <body>
+            <p>Dear, Student {student_number}</p>
+            <p style="margin-top: 0.2em; margin-bottom: 0.2em;">We appreciate your interest and dedication in applying for the <strong>{role}</strong> position on <strong>{election_name}</strong>.</p>
+            <p style="color: black;">After careful consideration, we regret to inform you that your candidacy has not been successful this time.</p>
+        </body>
+        </html>
+    """
+
+    body = ""
+    
+    if status == "Approved":
+        body = accept
+    else:
+        body = reject
+
+    msg.attach(MIMEText(body, 'html'))
+
+    # Send the email
+    server.send_message(msg)
+    server.quit()
+
+def send_partylist_status_email(party_email, status, partylist_name, election_name):
+    # Set up the SMTP server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
+
+    # Create the email
+    msg = MIMEMultipart()
+    msg['From'] = EMAIL
+    msg['To'] = party_email
+
+    if status == "Approved":
+        msg['Subject'] = "Confirmation of Partylist Approval"
+    else:
+        msg['Subject'] = "Regret on Partylist Application"
+
+    accept = f"""
+        <html>
+        <body>
+            <p>Greetings, fellow student</p>
+            <p style="margin-top: 0.2em; margin-bottom: 0.2em;">Congratulations! We are pleased to inform you that your partylist <strong>{partylist_name}</strong> on <strong>{election_name}</strong> has been approved.</p>
+        </body>
+        </html>
+    """
+
+    reject = f"""
+        <html>
+        <body>
+            <p>Greetings, fellow student</p>
+            <p style="margin-top: 0.2em; margin-bottom: 0.2em;">We appreciate your interest and dedication in applying for the <strong>{partylist_name}</strong> on <strong>{election_name}</strong>.</p>
+            <p style="color: black;">After careful consideration, we regret to inform you that your partylist has not been successful this time.</p>
+        </body>
+        </html>
+    """
+
+    body = ""
+    
+    if status == "Approved":
+        body = accept
+    else:
+        body = reject
+
+    msg.attach(MIMEText(body, 'html'))
+
+    # Send the email
+    server.send_message(msg)
+    server.quit()
