292273bb4082ba5ecfeeedf27c884f8060e9a0f3
Aizen1d <ddreataza@gmail.com>
2024-01-09 16:25:20 +0800

    New columns, preview and download button ui, function

--- resources/js/Pages/Comelec/DirectoryCertifications.vue
+++ resources/js/Pages/Comelec/DirectoryCertifications.vue
@@ -11,16 +11,27 @@
             <ActionButton @click="createCertification" class="create-button">Create</ActionButton>
         </div>
      
-        <BaseContainer class="item-container" :height="'auto'" :maxHeight="'550px'">
+        <BaseContainer class="item-container" :height="'auto'" :maxHeight="'75vh'">
             <BaseTable class="item-table" 
-                    :columns="['ID', 'Certification Title', 'Election Name', 'Date']" 
+                    :columns="['ID', 'Certification Title', 'Student Number', 'Election Title', 'Date Created', 'Preview / Download']" 
                     :tableHeight="'auto'"
-                    :maxTableHeight="'500px'">
-                <tr v-for="(certification, index) in certificationsData" :key="index" @click="selectItem(item)">
+                    :maxTableHeight="'69vh'">
+                <tr v-for="(certification, index) in certificationsData" :key="index">
                     <td class="my-cell">{{ index + 1 }}</td>
                     <td class="my-cell">{{ certification.Title }}</td>
+                    <td class="my-cell">{{ certification.StudentNumber }}</td>
                     <td class="my-cell">{{ certification.ElectionName }}</td>
                     <td class="my-cell">{{ toDate(certification.Date) }}</td>
+                    <td class="my-cell">
+                        <ActionButton 
+                            @click="previewCertification(certification)" class="preview-button mx-2">
+                            <i class="fas fa-eye"></i>
+                        </ActionButton>
+                        <ActionButton 
+                            @click="downloadCertification(certification)" class="download-button">
+                            <i class="fas fa-download"></i>
+                        </ActionButton>
+                    </td>
                 </tr>
             </BaseTable>
         </BaseContainer>
@@ -82,6 +93,30 @@
             toDate(date){
                 return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
             },
+            previewCertification(certification){
+                axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/certification/preview/${certification.CertificationId}`)
+                .then((response) => {
+                    window.open(response.data.pdf, '_blank');
+                })
+            },
+            downloadCertification(certification){
+                axios({
+                    url: `${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/certification/download/${certification.CertificationId}`,
+                    method: 'GET',
+                    responseType: 'blob', // important
+                })
+                .then((response) => {
+                    const url = window.URL.createObjectURL(new Blob([response.data]));
+                    const link = document.createElement('a');
+                    
+                    // Use the student number as the filename
+                    link.href = url;
+                    link.setAttribute('download', `${certification.StudentNumber}.pdf`); // replace with actual student number attribute
+
+                    document.body.appendChild(link);
+                    link.click();
+                })
+            }
         }
     }
 </script>
@@ -124,5 +159,24 @@
         border-radius: 7px;
         max-height: 550px;
         box-shadow: 0px 3px 5px rgba(167, 165, 165, 0.5);
+    }
+
+    .preview-button{
+        padding: 2% 8%;
+        border-radius: 10px;
+        background-color: #38a31b;
+    }
+    .download-button{
+        padding: 2% 8%;
+        border-radius: 10px;
+        background-color: #136ac2;
+    }
+
+    .preview-button:hover{
+        background-color: #2e8a14;
+    }
+
+    .download-button:hover{
+        background-color: #0f4e9c;
     }
 </style>
