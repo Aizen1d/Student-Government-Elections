da4333989a24d20aaf09c0a798b174885a488c3a
Aizen1d <ddreataza@gmail.com>
2024-01-17 01:41:01 +0800

    Unfinished but added for UI

--- resources/js/Pages/Winners.vue
+++ resources/js/Pages/Winners.vue
@@ -2,51 +2,126 @@
     <title>Winners - COMELEC Portal</title>
     <Navbar></Navbar>
 
-    <div class="main">
-        <div class="col" style="margin-left: 5%;">
-            <h1 class="path" v-if="!isElectionsLoading">
-                <span class="return" @click="returnPage">{{ electionsData.ElectionName }}</span>&nbsp;>&nbsp;Winners
-            </h1>
-        </div>
-        
-        <div class="header">
-            <h1 v-if="!isElectionsLoading" class="election-label">{{ electionsData.ElectionName }}</h1>
-            <h2 class="phrase">We extend our sincere congratulations to each of you on your election victories. Your leadership journeys have now officially begun.</h2>
-        </div>
+    <main class="main-margin">
+        <h1 class="current-page">
+            <span class="header" @click.prevent="returnElections">ONGOING ELECTIONS</span> 
+            <span class="arrow"> > </span>
+            <span class="header" @click.prevent="returnCurrentElection">{{ electionsData.election.ElectionName }}</span> 
+            <span class="arrow"> > </span>
+            Results
+        </h1>
 
-        <template v-if="!isElectionsLoading && !isWinnersLoading">
-            <div v-for="(winnerData, position) in winnersData" :key="position">
-                <h1 class="position candidate">{{ position }}</h1>
+        <div class="election">
+            <div class="election-wrapper">
+                <div class="election-header" v-if="!isElectionsLoading">
+                    <div class="centered">
+                        <img :src="electionsData.organization_logo" alt="" class="election-logo">
+                        <span class="election-title">{{ electionsData.election.ElectionName }}</span>
+                    </div>
+                </div>
 
-                <h1 class="candidate" v-if="winnerData.is_tied">
-                    (Tied)
-                </h1>
+                <hr class="line">
 
-                <div v-for="(candidate, candidateIndex) in winnerData.candidates" :key="candidateIndex" class="candidate">
-                    <div class="candidate-information-wrapper">
-                        <div class="candidate-information">
-                            <img :src="candidate.display_photo" alt="" class="candidate-photo">
-                            <h1 class="candidate-name">{{ candidate.full_name }}</h1>
-                            <h2 class="candidate-affiliation">{{ candidate.partylist }}</h2>
-                            <h2 class="candidate-affiliation">{{ candidate.votes }} votes</h2>
+                <div class="congratulations-card">
+                    <div class="congratulations-wrapper">
+                        <div class="congratulations-information">
+                            <img src="../../images/Winners/confetti.svg" alt="" class="congratulations-svg">
+                            <h1 class="message">We extend our sincere congratulations to each of you on your election victories! Your leadership journeys have now officially begun.</h1>
+                            <img src="../../images/Winners/fireworks.svg" alt="" class="congratulations-svg">
                         </div>
                     </div>
                 </div>
 
-                <!-- Display 'No winner' if there's no winner for this position -->
-                <div v-if="winnerData.no_winner" class="no-winner">
-                    <h1 class="candidate">
-                        (No winner)
-                    </h1>
+                <div class="card-set">
+                    <div class="card">
+                        <div class="card-wrapper">
+                            <div class="card-information">
+                                <img src="../../images/Winners/voters.svg" alt="" class="card-svg">
+                                <div class="count">
+                                    <span class="quantity">100</span>
+                                    <span class="quantity-label">Eligible Voters</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-wrapper">
+                            <div class="card-information">
+                                <img src="../../images/Winners/vote.svg" alt="" class="card-svg vote-svg">
+                                <div class="count">
+                                    <span class="quantity">70</span>
+                                    <span class="quantity-label">Votes</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-wrapper">
+                            <div class="card-information">
+                                <img src="../../images/Winners/abstain.svg" alt="" class="card-svg abstain-svg">
+                                <div class="count">
+                                    <span class="quantity">20</span>
+                                    <span class="quantity-label">Abstained</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <template v-if="!isElectionsLoading && !isWinnersLoading">
+                    <div v-for="(winnerData, position) in winnersData" :key="position">
+                        <div class="position-wrapper">
+                            <div class="position">
+                                <h1 class="position-label" v-if="!winnerData.is_tied">{{ position }}</h1>
+                                <h1 class="position-label" v-else>
+                                    {{ position }} - (Tied)
+                                </h1>
+                            </div>
+                        </div>
+
+                        <div v-if="winnerData.no_winner" class="top-ranks">
+                            <h1 class="candidate">
+                                (No winner)
+                            </h1>
+                        </div>
+
+                        <div class="top-ranks" v-for="(candidate, candidateIndex) in winnerData.candidates" :key="candidateIndex">
+                            <div class="top">
+                                <div class="top-wrapper">
+                                    <div class="top-information">
+                                        <div class="upload">
+                                            <img :src="candidate.display_photo" alt="" class="top-img">
+                                        </div>
+                                        <span class="top-name">{{ candidate.full_name }}</span>
+                                        <span class="top-affiliation">{{ candidate.partylist }}</span>
+                                        <span class="section">BSIT 3-1 (NOT YET CHANGED)</span>
+                                    </div>
+                                </div>
+                                <div class="stats row">
+                                    <div class="stats-count col-4">
+                                        <span class="count-quantity">{{ candidate.votes }}</span>
+                                        <img src="../../images/Winners/vote.svg" alt="" class="count-svg vote-svg">
+                                    </div>
+                                    <div class="stats-count col-4">                                
+                                        <span class="count-quantity">11</span>
+                                        <img src="../../images/Winners/abstain.svg" alt="" class="count-svg abstain-svg abstain">
+                                    </div>
+                                    <div class="stats-count col-4">                                
+                                        <span class="count-quantity">25</span>
+                                        <img src="../../images/Winners/percent.svg" alt="" class="count-svg percent">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <hr class="line">
+
+                    </div>
+                </template>
+                
             </div>
-        </template>
-        <template v-else>
-            <div class="skeleton">
-                <h3 class="candidate">LOADING..</h3>
-            </div>
-        </template>
-    </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -69,7 +144,7 @@
                 const response = await axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/election/view/${electionId.value}`);
                 console.log(`Get election with id ${electionId.value} successful. Duration: ${response.duration}ms`)
 
-                return response.data.election;
+                return response.data;
             }
 
             const { data: electionsData,
@@ -79,23 +154,6 @@
                 useQuery({
                     queryKey: [`getElectionData-${electionId.value}`],
                     queryFn: getElectionData,
-                })
-
-
-            const getPositionsFromElection = async () => {
-                const response = await axios.get(`${import.meta.env.VITE_FASTAPI_BASE_URL}/api/v1/election/view/${electionId.value}`);
-                console.log(`Get election with id ${electionId.value} successful. Duration: ${response.duration}ms`)
-
-                return response.data.positions;
-            }
-
-            const { data: positionsData,
-                isLoading: isPositionsLoading,
-                isSuccess: isPositionsSuccess,
-                isError: isPositionsError } =
-                useQuery({
-                    queryKey: [`getPositionsData-${electionId.value}`],
-                    queryFn: getPositionsFromElection,
                 })
 
             const getWinnersFromElection = async () => {
@@ -122,11 +180,6 @@
                 isElectionsSuccess,
                 isElectionsError,
 
-                positionsData,
-                isPositionsLoading,
-                isPositionsSuccess,
-                isPositionsError,
-
                 winnersData,
                 isWinnersLoading,
                 isWinnersSuccess,
@@ -144,7 +197,10 @@
             election_id: 0,
         },
         methods: {
-            returnPage(){
+            returnElections(){
+                router.visit(`/elections`)
+            },
+            returnCurrentElection(){
                 router.visit(`/elections/view?id=${this.electionId}`)
             },
         },
@@ -152,96 +208,294 @@
 </script>
 
 <style scoped>
-    .main{
+   .main-margin{
+        margin: 0% 8%;
+    }
+
+    .current-page{
         margin: 1.5% 0%;
-        font-family: 'Inter', sans-serif;
+        font-size: 28px;
+        font-weight: bold;
+        color: #800000 !important;
+    }
+
+    .arrow{
+        font-size: 28px;
+        font-weight: bold;
+        color: black !important;
     }
 
     .header{
-        text-align: center;
+        margin: 1.5% 0%;
+        font-size: 28px;
+        font-weight: bold;
+        color: black !important;
     }
 
-    .return{
-        font-weight: 700;
-        font-size: 30px;
-        color: #B90321;
-    }
-
-    .return:hover{
+    .header:hover{
         cursor: pointer;
         text-decoration: underline;
     }
 
-    .path{
-        font-weight: 700;
+    .centered{
+        display: flex;
+        align-items: center;
+    }
+
+    .end{
+        margin-left: auto;
+    }
+
+    .election{
+        margin: 1.5% 0%;
+    }
+
+    .election-wrapper{
+        padding: 2%;
+    }
+
+    .election-header{
+        align-items: center;
+    }
+
+    .election-logo{
+        width: 50px;
+    }
+
+    .election-title{
         font-size: 30px;
-        color: black;
+        font-weight: bold;
+        color: #800000;
+        margin: 0% 1.5%;
     }
 
     .election-label{
-        font-family: 'Inter Black', sans-serif;
-        font-weight: 700;
-        font-size: 35px;        
-        color: #B90321;
+        margin: 0% 1%;
+    }
+
+    .header-svg{
+        width: 35px;
+    }
+
+    .result{
+        filter: invert(74%) sepia(72%) saturate(2485%) hue-rotate(349deg) brightness(104%) contrast(88%);
+    }
+
+    .header-button{
+        border: transparent;
+        background-color: transparent;
+        align-items: center;
+    }
+
+    .line{
+        border: 0;
+        height: 2px;
+        background: rgb(249,249,249);
+        background: linear-gradient(90deg, rgba(249,249,249,1) 0%, rgba(217,217,217,1) 50%, rgba(249,249,249,1) 100%);
+        margin: 2% 0%;
+    }
+
+    .congratulations-card{
+        border-radius: 0%;
+        border: transparent;
+        background-color: #ffffff;
+        box-shadow: 0px 3px 5px rgba(167, 165, 165, 0.5);
+    }
+
+    .congratulations-wrapper{
+        padding: 1.5%;
+    }
+
+    .congratulations-information{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .congratulations-svg{
+        width: 100px;
+        margin: 0px 20px;
+        filter: brightness(0) saturate(100%) invert(62%) sepia(90%) saturate(630%) hue-rotate(1deg) brightness(105%) contrast(103%);
+    }
+
+    .message{
         margin: 0%;
-    }
-
-    .phrase{
-        font-size: 18px;
-        color: black;
-        margin: 1% 28%;
-    }
-
-    .candidate{
-        padding: 1%;
-        width: 100%;
         text-align: center;
+        font-size: 25px;
+    }
+
+    .card-set{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 2% 0%;
+    }
+
+    .card{
+        width: 32%;
+        border-radius: 0%;
+        border: transparent;
+        background-color: #ffffff;
+        box-shadow: 0px 3px 5px rgba(167, 165, 165, 0.5);
+    }
+
+    .card-wrapper{
+        padding: 3%;
+    }
+
+    .card-information{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0% 3%;
+    }
+
+    .card-svg{
+        width: 90px;
+    }
+
+    .count{
+        display: flex;
+        flex-direction: column;
+        text-align: end;
+    }
+
+    .quantity{
+        font-size: 50px;
+    }
+
+    .position-wrapper{
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        margin: 2% 0%;
     }
 
     .position{
-        font-weight: 800;
-        font-size: 30px;
-        margin: 0%;
-        text-transform: uppercase;
-        color: #B90321;
-        letter-spacing: 1px;
+        width: 32%;
+        background-color: #FFD966;
+        box-shadow: 0px 3px 5px rgba(167, 165, 165, 0.5);
+        text-align: center;
+        border-radius: 6px;
     }
 
-    .candidate-information-wrapper{
+    .position-label{
+        font-size: 28px;
+        font-weight: bold;
+        padding: 3%;
+        margin: 0%;
+    }
+
+    .top-ranks{
         display: flex;
+        align-items: center;
         justify-content: center;
-        margin: 0%;
-        width: 100%;
-        flex-wrap: wrap;
     }
 
-    .candidate-information{
-        width: 300px;
-        height: 300px;
-        padding: 1%;
+    .top{
+        width: 32%;
+        height: 400px;
+        text-align: center;
+        background-color: white;
+        box-shadow: 0px 3.5px 5px rgba(167, 165, 165, 0.5);
+        border-radius: 6px;
+        display: flex; 
+        flex-direction: column; 
+        justify-content: space-between; 
+        align-items: center; /* Add this */
+        margin: 0% 1%;
+    }
+
+    .top-wrapper{
+        padding: 3%;
+        display: flex; /* Add this */
+        flex-direction: column; /* Add this */
+        justify-content: center; /* Add this */
+        align-items: center; /* Add this */
+        margin-top: 40px;
+    }
+
+    .top-information{
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
-        flex-direction: column;
-        margin: 1% 1%;
     }
 
-    .candidate-photo{
-        width: 170px;
-        height: 170px;
-        margin-top: -10%;
-        margin-bottom: 5%;
+    .top-img{
+        width: 160px;
+        height: 160px;
         border-radius: 50%;
         object-fit: cover;
     }
 
-    .candidate-name{
-        font-size: 20px;
-        margin: 3% 0%;
-        font-weight: 800;
+    .upload{
+        margin-top: -20px;
     }
 
-    .candidate-affiliation{
+    .rank-svg{
+        width: 38px;
+        height: 38px;
+        margin-bottom: 10px;
+    }
+
+    .top-name{
+        font-weight: bold;
+        margin-top: 10px;
+        margin-bottom: 10px;
         font-size: 18px;
+    }
+
+    .top-affiliation{
+        margin-bottom: 10px;
+    }
+
+
+    .stats{
+        width: 100%; /* Add this */
+        text-align: center; /* Add this */
+        display: flex;
+        align-items: center;
+        justify-content: space-around; /* Change this */
+        margin: 1% 0%;
+        margin-bottom: 0%;
+        background-color: white;
+    }
+
+    .stats-count{
+        padding: 1.5%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #E7E6E6;
+    }
+
+    .count-svg{
+        width: 35px;
+        filter: invert(75%) sepia(0%) saturate(132%) hue-rotate(164deg) brightness(91%) contrast(82%);
+    }
+
+    .count-quantity{
+        font-size: 22px;
+        margin: 0px 10px;
+        font-weight: bold;
+    }
+
+    .percent{
+        width: 20px;
+        margin: 7.5px;
+    }
+
+    .vote-svg{
+        filter: invert(56%) sepia(65%) saturate(372%) hue-rotate(52deg) brightness(96%) contrast(89%);
+    }
+
+    .abstain-svg{
+        filter: brightness(0) saturate(100%) invert(35%) sepia(30%) saturate(1936%) hue-rotate(320deg) brightness(106%) contrast(106%);
+    }
+
+    .abstain{
+        width: 30px;
+        margin: 2.5px;
     }
 </style>
---
+++ resources/js/Shared/LabelToolTip.vue
@@ -0,0 +1,45 @@
+<template>
+    <div style="position: relative; display: inline-block;">
+    
+        <div v-show="showInfo" style="padding: 3px 3px; background-color: rgb(36, 36, 36); position: absolute; left: 140%; top: 50%; transform: translateY(-50%); border: white 1px solid; border-top-left-radius: 7px; border-bottom-left-radius: 7px; max-width: fit-content;">
+            <p v-show="showInfo" class="tool" style="text-align: justify;">
+                <slot></slot>
+            </p>
+            <div v-show="showInfo" class="tooltip-arrow"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                showInfo: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .tooltip-arrow {
+        width: 0;
+        height: 0;
+        border-top: 7px solid transparent;
+        border-bottom: 7px solid transparent;
+        border-right: 7px solid rgb(36, 36, 36);
+        position: absolute;
+        top: 50%;
+        left: -6px;
+        transform: translateY(-50%);
+    }
+
+    .tool {
+        color: white;
+        font-size: 12px;
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 5px;
+        word-wrap: break-word; /* Enable word wrapping */
+        max-width: 350px; /* Optional: Set a maximum width for the tooltip */
+    }
+</style>
